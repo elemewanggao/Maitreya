d800673d0cfdb8b6ddde47dffdc587dd6c56c79a
wanggao <wanggao@wanggaodeMacBook-Pro.local>
2017-08-21 21:41:50 +0800

    add log

--- Maitreya/myapp.py
+++ Maitreya/myapp.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 """app入口，用gunicorn直接启动."""
-
+import logging.config
 from flask import Flask
 from gevent import monkey
 from flask_cors import CORS
-from Maitreya.settings import CORS_CONF
+from Maitreya.settings import CORS_CONF, LOGGING
 from Maitreya.router import route_init
 
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app, resources=CORS_CONF, supports_credentials=True)
 monkey.patch_all()
 
-
+logging.config.dictConfig(LOGGING)
 route_init(app)
 
 if __name__ == '__main__':
--- Maitreya/settings.py
+++ Maitreya/settings.py
@@ -4,3 +4,35 @@
 
 # 跨域请求配置
 CORS_CONF = {r'/*': {'origins': '*'}}
+
+# 日志设置
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'handlers': ['all', 'console'],
+        'level': 'INFO'
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'uniform',
+        },
+        'all': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'uniform',
+            'filename': '/data/log/Maitreya/maitreya.log',
+            'mode': 'a',
+            'maxBytes': 262144000,  # 200M
+            'backupCount': 10,
+        },
+    },
+    'formatters': {
+        'uniform': {
+            'format': '%(asctime)s %(levelname)-6s %(name)s[%(process)d]: '
+                      '%(module)s => %(funcName)s ##  %(message)s',
+        }
+    }
+}
